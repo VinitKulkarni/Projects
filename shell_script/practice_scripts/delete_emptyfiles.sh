f36077ab682517e6b6d42484a812480758a2a1ce
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-04-05 17:50:34 +0530

    Update delete_emptyfiles.sh

--- shell_script/practice_scripts/delete_emptyfiles.sh
+++ shell_script/practice_scripts/delete_emptyfiles.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 #delete the empty files from the directory
 
+dir=/home/ubuntu/shell_script
+listOfFiles=$( ls -l $dir | awk '{if ($5 == 0) print $9}' )
+
+count=0
+for item in $listOfFiles
+do
+        let count++
+        fileName=$(echo $item | awk '{printf $1}')
+        echo $fileName
+        rm -f $fileName
+done
+
+if [ $count -gt 0 ]
+then
+        echo "These files are removed from" $dir
+else
+        echo "No empty files in this" $dir
+fi
+
+
