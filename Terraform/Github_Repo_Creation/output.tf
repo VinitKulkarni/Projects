78dd3017ed6dc2af624b3c00617aeef644498ee6
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-04-06 21:29:47 +0530

    Create output.tf

---
+++ Terraform/Github_Repo_Creation/output.tf
@@ -0,0 +1,15 @@
+output "github_repo_fullname_1" {
+  value = github_repository.repo_creation_1.full_name
+}
+
+output "github_url_1" {
+  value = github_repository.repo_creation_1.html_url
+}
+
+output "github_url_2" {
+  value = github_repository.repo_creation_2.html_url
+}
+
+output "github_repo_fullname_2" {
+  value = github_repository.repo_creation_2.full_name
+}
