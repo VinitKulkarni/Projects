04d2c7e2742a2c77ef6a4a455c03b65311bee2bf
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-04-06 21:40:20 +0530

    Create variables.tf

---
+++ Terraform/VPC_EC2_Creation/variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  type = string
+}
+
+variable "cidr_block" {
+    type = string
+}
+
+variable "ami" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "availability_zone_public_subnet" {
+    type = string
+}
+
+variable "availability_zone_private_subnet" {
+    type = string
+}
+
+variable "publicSubnet_cidr_block" {
+    type = string
+}
+
+variable "privateSubnet_cidr_block" {
+  type = string
+}
