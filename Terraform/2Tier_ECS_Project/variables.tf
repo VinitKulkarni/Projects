a51d74745127e97932da2126b92e6f22886327cf
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-07-03 12:13:46 +0530

    Create variables.tf

---
+++ Terraform/2Tier_ECS_Project/variables.tf
@@ -0,0 +1,27 @@
+variable "availability_zone_1" {
+  type = string
+}
+
+variable "availability_zone_2" {
+  type = string
+}
+
+variable "frontend_image" {
+  type = string
+}
+
+variable "backend_image" {
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "publicSubnet1a_cidr_block" {
+  type = string
+}
+
+variable "publicSubnet1b_cidr_block" {
+  type = string
+}
