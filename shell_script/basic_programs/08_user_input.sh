1f0a133c2f79e1263512a948ed7f6bcc53d62290
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-07-03 12:32:54 +0530

    taking input from user

---
+++ shell_script/basic_programs/08_user_input.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#user input
+
+#type1
+read -p "Enter the name:" name
+read -p "Enter the city:" city
+echo "name=$name and city=$city"
+
+#type2
+echo "enter the number"
+read number
+echo "you have entered this $number"
+
+#type3
+read anything
+echo "you have entered: $anything"
