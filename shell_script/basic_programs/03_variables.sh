28a29cce3c025b3dc692934aa20c6c3f8123c1d7
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-07-03 12:28:05 +0530

    working with variables

---
+++ shell_script/basic_programs/03_variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#declare variables
+age=28
+name="vinit"
+city="banglore"
+
+#print variable values
+echo "name="$name" age="$age" city="$city
+
+#assing value to variable
+temp=10
+a=$temp
+echo $a
+
+#constant variable
+readonly PI_VALUE=3.142
+echo "PI value = $PI_VALUE"
+
+# PI_VALUE=4 you will get error if try to change
