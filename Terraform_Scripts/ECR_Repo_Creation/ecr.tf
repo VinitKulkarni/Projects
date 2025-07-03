e9f695b15e9f6d86a6f7cf961aca43b5489ae9b4
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-07-03 12:19:30 +0530

    Delete Terraform_Scripts/ECR_Repo_Creation directory

--- Terraform_Scripts/ECR_Repo_Creation/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "flask_repo" {
-  name                 = "flask-backend" #This is the name you see in ECR
-  image_tag_mutability = "MUTABLE"
-}
-
-resource "aws_ecr_repository" "express_repo" {
-  name                 = "express-frontend" #This is the name you see in ECR
-  image_tag_mutability = "MUTABLE"
-}
